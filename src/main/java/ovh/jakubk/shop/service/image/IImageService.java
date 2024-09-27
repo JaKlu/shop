f06035c3b94d198c5bfa326b9a55a7844d9dbfab
JaKlu <kluska.jakub@gmail.com>
2024-09-27 12:26:46 +0200

    imageDto, image service

--- src/main/java/ovh/jakubk/shop/service/image/IImageService.java
+++ src/main/java/ovh/jakubk/shop/service/image/IImageService.java
@@ -1,14 +1,17 @@
 package ovh.jakubk.shop.service.image;
 
 import org.springframework.web.multipart.MultipartFile;
+import ovh.jakubk.shop.dto.ImageDto;
 import ovh.jakubk.shop.model.Image;
+
+import java.util.List;
 
 public interface IImageService {
     Image getImageById(Long id);
 
     void deleteImageById(Long id);
 
-    Image saveImage(MultipartFile file, Long productId);
+    List<ImageDto> saveImages(List<MultipartFile> files, Long productId);
 
     void updateImage(MultipartFile file, Long imageId);
 }
--- src/main/java/ovh/jakubk/shop/service/image/ImageService.java
+++ src/main/java/ovh/jakubk/shop/service/image/ImageService.java
@@ -3,14 +3,18 @@ package ovh.jakubk.shop.service.image;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import ovh.jakubk.shop.dto.ImageDto;
 import ovh.jakubk.shop.exceptions.ResourceNotFoundException;
 import ovh.jakubk.shop.model.Image;
+import ovh.jakubk.shop.model.Product;
 import ovh.jakubk.shop.repository.ImageRepository;
 import ovh.jakubk.shop.service.product.IProductService;
 
 import javax.sql.rowset.serial.SerialBlob;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -34,7 +38,40 @@ public class ImageService implements IImageService {
     }
 
     @Override
-    public Image saveImage(MultipartFile file, Long productId) {
+    public List<ImageDto> saveImages(List<MultipartFile> files, Long productId) {
+        Product product = productService.getProductById(productId);
+        List<ImageDto> savedImageDto = new ArrayList<>();
+
+        for (MultipartFile file : files) {
+            try {
+                Image image = new Image();
+                image.setFileName(file.getName());
+                image.setFileType(image.getFileType());
+                image.setImage(new SerialBlob(file.getBytes()));
+                image.setProduct(product);
+
+                String buildDownloadUrl = "/api/v1/images/image/download/";
+                String downloadUrl = buildDownloadUrl + image.getId();
+                image.setDownloadUrl(downloadUrl);
+
+                Image savedImage = imageRepository.save(image);
+
+                savedImage.setDownloadUrl(buildDownloadUrl + savedImage.getId());
+                imageRepository.save(savedImage);
+
+                ImageDto imageDto = new ImageDto();
+                imageDto.setImageId(savedImage.getId());
+                imageDto.setImageName(savedImage.getFileName());
+                imageDto.setDownloadUrl(savedImage.getDownloadUrl());
+
+                savedImageDto.add(imageDto);
+            } catch (IOException | SQLException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+            return savedImageDto;
+        }
+
+
         return null;
     }
 
