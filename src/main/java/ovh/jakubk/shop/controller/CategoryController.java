2ca6f841b5c37d18f0b04675af00e3537171c487
JaKlu <kluska.jakub@gmail.com>
2024-09-27 14:14:11 +0200

    image controller

---
+++ src/main/java/ovh/jakubk/shop/controller/CategoryController.java
@@ -0,0 +1,4 @@
+package ovh.jakubk.shop.controller;
+
+public class CategoryController {
+}
---
+++ src/main/java/ovh/jakubk/shop/response/ApiResponse.java
@@ -0,0 +1,11 @@
+package ovh.jakubk.shop.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class ApiResponse {
+    private String message;
+    private Object data;
+}
